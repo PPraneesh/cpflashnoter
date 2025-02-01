a53a855154648f67ce3fe629f35743d156be9dcf
Praneesh <parshipraneesh8@gmail.com>
2025-02-01 13:35:19 +0530

    fix

--- BACKEND/src/config/razorpay.js
+++ BACKEND/src/config/razorpay.js
@@ -2,8 +2,8 @@ const Razorpay = require("razorpay");
 require("dotenv").config();
 
 var razorpay = new Razorpay({
-    key_id: process.env.RAZORPAY_API_KEY,
-    key_secret: process.env.RAZORPAY_API_SECRET,
+    key_id: process.env.RAZORPAY_API_KEY_LIVE,
+    key_secret: process.env.RAZORPAY_API_SECRET_LIVE,
   });
 
 module.exports = razorpay;
